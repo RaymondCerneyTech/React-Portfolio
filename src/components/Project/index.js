16f72fe31ca22cf32ec7b4c19e58fe38bb24ee46
raymondcerneytech <cerne004@gmail.com>
2022-02-02 21:22:52 -0600

    added components

---
+++ src/components/Project/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+function Body() {
+	return (
+		
+    <div class="new-section">
+        <div class="title" id="about">About</div>
+        <div class="about-info">Welcome! I'm Ray, an aspiring full stack developer and software engineer.
+
+          <br /><br /> I have a bachelor's in Computer Science and will be
+          completing a full stack web development
+          bootcamp in February; both from The University of Minnesota.
+
+          <br /><br /> I was a manual software tester for nearly 3 years at
+          Lifetouch Photography, testing both
+          field-facing and finance department web apps.
+          <br /> When companies started going remote due to covid, my employer
+          laid off all of their manual testers,
+          which was the perfect reason for me to continue my education.
+
+          <br /><br /> The satisfaction received by engineering and developing
+          automated solutions to people's problems
+          is the reason I'm seeking a career in computer science.
+        </div>
+      </div>
+
+      
+      <div class="new-section">
+        <div class="title" id="projects">Projects</div>
+
+        
+        <div class="content">
+				<a title="main-project-title" class="project-link" href="https://ahmed-sajjad111.github.io/Munchies-and-Movies/"><img src="./assets/images/Movies-and-Munchies-700.png" alt="Movies and Munchies Image">
+				</a>
+
+          
+          <a title="project-two-title" class="project-link"
+            href="https://cbc-my-book.herokuapp.com/">
+            <img class="secondary-projects" src="./assets/images/MyBook.png"
+              alt="MyBook Site Image">
+          </a>
+
+          <a title="project-three-title" class="project-link" href="">
+            <img class="secondary-projects"
+              src="./assets/images/Project-Three.png" alt="Project Three Image">
+          </a>
+
+
+          
+            <a title="project-four-title" class="project-link" href="">
+              <img class="secondary-projects"
+                src="./assets/images/Project-Four.png" alt="Project Four Image">
+            </a>
+
+            <a title="project-five-title" class="project-link" href="">
+              <img class="secondary-projects"
+                src="./assets/images/Project-Five.png" alt="Project Five Image">
+            </a>
+
+          
+        </div>
+      </div>
+	);
+}
+
+export default Body;
