a2f8bbe40b1daa335587db3484fdbe740c2f02e0
raymondcerneytech <cerne004@gmail.com>
2022-02-02 21:25:48 -0600

    updated footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,9 +2,59 @@ import React from "react";
 
 function Footer() {
 	return (
-		<section>
-			<h1 id="about">Who am I?</h1>
-		</section>
+		<div class="new-section foot">
+        
+        <div class="title" id="contact">Contact</div>
+
+
+        
+        <div class="footer-small">
+          <span class="footer-link">
+            <a href="cerne004@gmail.com"> Email </a>
+          </span>
+        
+          <span class="footer-link">
+            <a href="https://github.com/RaymondCerneyTech">
+              GitHub
+            </a>
+          </span>
+        
+          <span class="footer-link">
+            <a href="https://www.linkedin.com/in/raymondcerney/">
+              LinkedIn
+            </a>
+          </span>
+        </div>
+
+        
+        <div class="footer-large">
+          <div>
+            Email:
+            <span class="footer-link">
+              cerne004@gmail.com
+            </span>
+          </div>
+        
+          <div>
+            GitHub:
+            <span class="footer-link">
+              <a href="https://github.com/RaymondCerneyTech">
+                https://github.com/RaymondCerneyTech
+              </a>
+            </span>
+          </div>
+          
+          <div>
+            LinkedIn:
+            <span class="footer-link">
+              <a href="https://www.linkedin.com/in/raymondcerney/">
+                https://www.linkedin.com/in/raymondcerney/
+              </a>
+            </span>
+          </div>
+        </div>
+
+      </div>
 	);
 }
 
