fcc66e27ad083c29412ed0aebba961681523bfd4
raymondcerneytech <cerne004@gmail.com>
2022-02-02 21:43:40 -0600

    updated Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,40 @@ import React from "react";
 
 function Header() {
 	return (
-		<section>
-			<h1 id="about">Who am I?</h1>
-		</section>
+		<header class="header-content">
+			<div class="header-top">
+				<h1 class="header-name">Raymond Cerney</h1>
+				<nav>
+					<ul>
+						<li>
+							<a href="#about">About Me</a>
+						</li>
+						<li>
+							<a href="#projects">Projects</a>
+						</li>
+						<li>
+							<a href="#contact">Contact</a>
+						</li>
+						<li>
+							<a href="./assets/resume/Web Development Resume.docx" download="Web Development Resume.docx" target="_blank">
+								Download Resume
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<div class="header-bottom">
+				<div class="bottom-head header-two">
+					<div class="h-2-text">Master</div>
+				</div>
+				<div class="bottom-head header-three">
+					<div class="h-3-text">Mentor</div>
+				</div>
+				<div class="bottom-head header-four">
+					<div class="h-4-text"></div>Unify
+				</div>
+			</div>
+		</header>
 	);
 }
 
